c650608ad6732e343c21085e2f8fde6c47302f0f
reno finsa <reno.finsa@gmail.com>
2020-02-26 04:41:11 +0700

    redux-persistor, localStorage on React Apps

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import store from './redux/store'
+import { store, persistor } from './redux/store'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate >
         </BrowserRouter>
     </Provider>
 , document.getElementById('root'));
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
 
 import indexReducer from './index'
 
 const middlewares = [logger]
 
-const store = createStore(indexReducer, applyMiddleware(...middlewares)
+export const store = createStore(indexReducer, applyMiddleware(...middlewares)
 +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-export default store
+export const persistor = persistStore(store)
+
+export default { store, persistor }
