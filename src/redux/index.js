10577f175687c9a1c94959dc3def850b47d65f66
reno finsa <reno.finsa@gmail.com>
2020-02-26 06:11:23 +0700

    Directory State with Redux

--- src/redux/index.js
+++ src/redux/index.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage'
 
 import userReducer from './reducer/user'
 import cartReducer from './reducer/cart'
+import directoryReducer from './reducer/directory'
 
 const persistConfig = {
     key: 'root',
@@ -13,7 +14,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 })
 
 export default persistReducer(persistConfig, rootReducer)
